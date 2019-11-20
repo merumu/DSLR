8f7b7dbc7ee14faf4ede4b8e0d16fc7bac871fae
Martin LEROY <maleroy@student.42.fr>
2019-11-20 13:17:50 +0100

    std() min() max()

--- describe.py
+++ describe.py
@@ -13,23 +13,60 @@ class FileLoader:
         return df
 
 def count(data):
-    n = 0
+    total = 0
     for value in data:
-        n += value
-    return n
+        total += value
+    return total
 
 def mean(data):
+    total = count(data)
+    return total / len(data)
 
+def std(data):
+    n = 0
+    summ = 0
+    moy = mean(data)
+    for value in data:
+        summ += (value - moy) ** 2
+        n += 1
+    return (summ / n) ** 1/2
 
+def mini(data):
+    tmp = data[0]
+    for value in data:
+        if tmp > value:
+            tmp = value
+    return tmp
+
+def maxi(data):
+    tmp = data[0]
+    for value in data:
+        if tmp < value:
+            tmp = value
+    return tmp
+
+def firstQuartile(data):
+    #tmp = data.sort_values()
+    #n = len(tmp)
+    #print((n + 3) // 4)
+    #print(tmp[(n + 3) // 4])
+    return 0
+
+def median():
+    pass
+
+def thirdQuartile():
+    pass
 
 def describe(data):
     feature = pandas.DataFrame(index=['Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max'])
     n = 1
     for row in data:
         if all(isinstance(x, float) for x in data[row]):
-            tmp = data.dropna()
-            feature['Feature ' + str(n)] = [count(tmp), mean(tmp), 3, 4, 5, 6, 7, 8]
-            n += 1
+            tmp = data[row].dropna()
+            if len(tmp) > 0:
+                feature['Feature ' + str(n)] = [count(tmp), mean(tmp), std(tmp), mini(tmp), firstQuartile(tmp), 6, 7, maxi(tmp)]
+                n += 1
     print(feature)
 
 if __name__ == "__main__":
