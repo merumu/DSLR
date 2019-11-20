d1d6a5dbe13f5b07aa62a45489a457032ab2ee72
Martin LEROY <maleroy@student.42.fr>
2019-11-20 16:18:04 +0100

    plt.show histogram

--- histogram.py
+++ histogram.py
@@ -1,10 +1,17 @@
 import sys
+import numpy as np
 from FileLoader import FileLoader
+import matplotlib.pyplot as plt
 
 def histogram(data):
     for row in data:
         if all(isinstance(x, float) for x in data[row]):
-            print("draw histogram of " + str(row))
+            tmp = data[row].dropna()
+            if len(tmp) > 0:
+                a = np.hstack(tmp)
+                _ = plt.hist(a, bins='auto')
+                plt.title(str(row))
+                plt.show()
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
