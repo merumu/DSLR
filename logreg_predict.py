74bab593e4bb16f2e04602b6283aefa7db831e10
Martin LEROY <maleroy@student.42.fr>
2020-02-11 15:40:37 +0100

    normalization needed

--- logreg_predict.py
+++ logreg_predict.py
@@ -3,6 +3,7 @@ import pickle
 from FileLoader import FileLoader
 import numpy as np
 from logreg_train import setTheta
+import csv
 
 def getTheta(size):
     try:
@@ -20,10 +21,10 @@ def getTheta(size):
         thetaH = np.zeros(size)
     return (thetaG, thetaR, thetaS, thetaH)
 
-def delete_nan(stud, thetaG, thetaR, thetaS, thetaH):
+def delete_nan(size, stud, thetaG, thetaR, thetaS, thetaH):
     delete = 0
-    for n in range(5):
-        if n + delete < 5 and np.isnan(stud[n]):#delete nan in stud and corresponding thetas
+    for n in range(size):
+        if n + delete < size and np.isnan(stud[n]):#delete nan in stud and corresponding thetas
             stud = np.delete(stud, n)
             thetaG = np.delete(thetaG, n + 1)
             thetaR = np.delete(thetaR, n + 1)
@@ -31,24 +32,39 @@ def delete_nan(stud, thetaG, thetaR, thetaS, thetaH):
             thetaH = np.delete(thetaH, n + 1)
             n -= 1
             delete += 1
-    stud = stud.reshape((1,5 - delete))
+    stud = stud.reshape((1, size - delete))
     return (stud, thetaG, thetaR, thetaS, thetaH)
 
 def predict(data):
     x_test = data[['Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Ancient Runes', 'Charms']].to_numpy()
-    for stud in x_test:
-        (thetaG, thetaR, thetaS, thetaH) = getTheta(stud.shape[0] + 1)
-        (stud, thetaG, thetaR, thetaS, thetaH) = delete_nan(stud, thetaG, thetaR, thetaS, thetaH)
-        stud = np.insert(stud, 0, 1, axis=1)
-        g_score = stud.dot(thetaG)
-        r_score = stud.dot(thetaR)
-        s_score = stud.dot(thetaS)
-        h_score = stud.dot(thetaH)
-        print("score :")
-        print(g_score)
-        print(r_score)
-        print(s_score)
-        print(h_score)
+    #x_test = data[['Arithmancy','Astronomy','Herbology','Defense Against the Dark Arts','Divination','Muggle Studies','Ancient Runes','History of Magic','Transfiguration','Potions','Care of Magical Creatures','Charms','Flying']].to_numpy()
+    with open('houses.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Index', 'Hogwarts House']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        n = 0
+        for stud in x_test:
+            (thetaG, thetaR, thetaS, thetaH) = getTheta(stud.shape[0] + 1)
+            (stud, thetaG, thetaR, thetaS, thetaH) = delete_nan(x_test.shape[1], stud, thetaG, thetaR, thetaS, thetaH)
+            stud = np.insert(stud, 0, 1, axis=1)
+            g_score = stud.dot(thetaG)
+            r_score = stud.dot(thetaR)
+            s_score = stud.dot(thetaS)
+            h_score = stud.dot(thetaH)
+            print("score :")
+            print(g_score)
+            print(r_score)
+            print(s_score)
+            print(h_score)
+            if max([g_score, r_score, s_score, h_score]) == g_score:
+                writer.writerow({'Index': n, 'Hogwarts House': 'Gryffindor'})
+            elif max([g_score, r_score, s_score, h_score]) == r_score:
+                writer.writerow({'Index': n, 'Hogwarts House': 'Ravenclaw'})
+            elif max([g_score, r_score, s_score, h_score]) == s_score:
+                writer.writerow({'Index': n, 'Hogwarts House': 'Slytherin'})
+            elif max([g_score, r_score, s_score, h_score]) == h_score:
+                writer.writerow({'Index': n, 'Hogwarts House': 'Hufflepuff'})
+            n += 1
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
--- logreg_train.py
+++ logreg_train.py
@@ -16,26 +16,26 @@ def setTheta(thetaG, thetaR, thetaS, thetaH):
 def training(data):
     data = data.dropna()#for later : should drop only NaN line in selected columns
     x_train = data[['Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Ancient Runes', 'Charms']].to_numpy()
+    #x_train = data[['Arithmancy','Astronomy','Herbology','Defense Against the Dark Arts','Divination','Muggle Studies','Ancient Runes','History of Magic','Transfiguration','Potions','Care of Magical Creatures','Charms','Flying']].to_numpy()
     y = np.array(data['Hogwarts House'])
-
-    gryffindor = LogisticRegression(alpha=0.0000001, max_iter=150, verbose=True, learning_rate='constant')
+    gryffindor = LogisticRegression(alpha=0.0001, max_iter=100, verbose=True, learning_rate='constant')
     y_train = np.where(y == 'Gryffindor', 1, 0)
     #np.set_printoptions(threshold=sys.maxsize)
     #print(y_train)
     gryffindor.fit(x_train, y_train)
     print(gryffindor.thetas)
 
-    ravenclaw = LogisticRegression(alpha=0.0000001, max_iter=150, verbose=True, learning_rate='constant')
+    ravenclaw = LogisticRegression(alpha=0.0001, max_iter=100, verbose=True, learning_rate='constant')
     y_train = np.where(y == 'Ravenclaw', 1, 0)
     ravenclaw.fit(x_train, y_train)
     print(ravenclaw.thetas)
 
-    slytherin = LogisticRegression(alpha=0.0000001, max_iter=150, verbose=True, learning_rate='constant')
+    slytherin = LogisticRegression(alpha=0.0001, max_iter=100, verbose=True, learning_rate='constant')
     y_train = np.where(y == 'Slytherin', 1, 0)
     slytherin.fit(x_train, y_train)
     print(slytherin.thetas)
 
-    hufflepuff = LogisticRegression(alpha=0.0000001, max_iter=150, verbose=True, learning_rate='constant')
+    hufflepuff = LogisticRegression(alpha=0.0001, max_iter=100, verbose=True, learning_rate='constant')
     y_train = np.where(y == 'Hufflepuff', 1, 0)
     hufflepuff.fit(x_train, y_train)
     print(hufflepuff.thetas)
